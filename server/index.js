9b4ec6c86d24e9a12c2c183126203c263683d577
IsraeLasrY <93082806+IsraeLasrY@users.noreply.github.com>
2024-10-15 19:24:29 +0300

    change delete

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-import express from "express";
-import { connect } from "mongoose";
-import coockieSession from "cookie-session";
-import { initialize, session } from "passport";
-import { mongooURI, cookieKay } from "./config/keys";
-import "./models/User";
-import "./services/passport";
+const express = require("express");
+const mongoose = require("mongoose");
+const coockieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
 
-connect(mongooURI);
+mongoose.connect(keys.mongooURI);
 
 const app = express();
 
 app.use(
   coockieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [cookieKay],
+    keys: [keys.cookieKay],
   })
 );
-app.use(initialize());
-app.use(session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/autoRoutes")(app);
 const PORT = process.env.PORT || 5000;
