8da4fdd6ed9e93d97a78fb4e3f2ef89124cec513
IsraeLasrY <93082806+IsraeLasrY@users.noreply.github.com>
2024-10-15 14:51:53 +0300

    Authentiction part
    Push all part of Authentication in project

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const coockieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
+mongoose.connect(keys.mongooURI);
+
 const app = express();
+
+app.use(
+  coockieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKay],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/autoRoutes")(app);
 const PORT = process.env.PORT || 5000;
--- server/routes/autoRoutes.js
+++ server/routes/autoRoutes.js
@@ -8,4 +8,15 @@ module.exports = (app) => {
   );
   //callback func after we get the info from google, put user on hold and take the 'code' from URL
   app.get("auth/google/callback", passport.authenticate("google"));
+
+  //Logs the user out and tries to send the current (now logged-out) user information (likely null).
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  //Returns the session information, which contains data about the currently authenticated user, if any.
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.session);
+  });
 };
